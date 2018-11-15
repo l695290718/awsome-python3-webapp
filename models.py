b8c7a8788388791f958ad25514922fdf66f2a362
Ramberiu <695290718@qq.com>
2018-11-15 17:24:44 +0800

    2018.11.15.17:24

---
+++ models.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+# -*- encoding: utf-8 -*-
+
+"""
+@Author  :   {Cassiellb}
+
+@License :   (C) Copyright 2013-2017, {Nanjing University of posts and Telecommunications}
+
+@Contact :   {695290718@qq.com}
+
+@Software:   PyCharm
+
+@File    :   models.py
+
+@Time    :   2018/11/15 16:24
+
+@Desc    :
+
+"""
+import time
+import uuid
+
+from orm import Model, StringField, BooleanField, FloatField, TextField
+
+
+def next_id():
+    return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
+
+
+class User(Model):
+    __table__ = 'users'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    email = StringField(ddl='varchar(50)')
+    passwd = StringField(ddl='varchar(50)')
+    admin = BooleanField()
+    name = StringField(ddl='varchar(50)')
+    image = StringField(ddl='varchar(500)')
+    created_at = FloatField(default=time.time)
+
+
+class Blog(Model):
+    __table__ = 'blogs'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    name = StringField(ddl='varchar(50)')
+    summary = StringField(ddl='varchar(200)')
+    content = TextField()
+    created_at = FloatField(default=time.time)
+
+
+class Comment(Model):
+    __table__ = 'comments'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    blog_id = StringField(ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    content = TextField()
+    created_at = FloatField(default=time.time)
+
